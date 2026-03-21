c599a5f59d285d114da2e3f80666ef60e3b21569
wilabarca <wilberabarcasanchez078@gmail.com>
2026-03-21 00:40:50 -0600

    depedeni

---
+++ api/index.ts
@@ -0,0 +1,14 @@
+import serverless from 'serverless-http';
+import app, { initializeRoutes } from '../src/app';
+
+let initialized = false;
+
+const handler = serverless(app);
+
+export default async (req: any, res: any) => {
+  if (!initialized) {
+    await initializeRoutes();
+    initialized = true;
+  }
+  return handler(req, res);
+};
