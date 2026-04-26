423eb433cc24b94e046f80f28d950bfceabc2510
Luis1f2 <221271@ids.upchiapas.edu.mx>
2026-04-26 11:03:34 -0600

    Se_corrigio_errores_por_segunda_vez

--- src/Patient/Infrastructure/Router/PatientRouters.ts
+++ src/Patient/Infrastructure/Router/PatientRouters.ts
@@ -2,30 +2,30 @@ import { Router } from "express";
 import { PatientController } from "../Controllers/PatientController";
 
 export function RegisterPatientRoutes(
-        router: Router,
-        patientController: PatientController
-    ): void {
-        const patientGroup = Router();
+  router: Router,
+  patientController: PatientController
+): void {
+  const patientGroup = Router();
 
-    patientGroup.post("/", (req, res) =>
-        patientController.createPatient(req, res)
-    );
+  patientGroup.post("/", (req, res) =>
+    patientController.createPatient(req, res)
+  );
 
-    patientGroup.get("/:id", (req, res) =>
-        patientController.getPatientById(req, res)
-    );
+  patientGroup.get("/caregiver/:caregiverUserId", (req, res) =>
+    patientController.getPatientsByCaregiver(req, res)
+  );
 
-    patientGroup.get("/caregiver/:caregiverUserId", (req, res) =>
-        patientController.getPatientsByCaregiver(req, res)
-    );
+  patientGroup.get("/:id", (req, res) =>
+    patientController.getPatientById(req, res)
+  );
 
-    patientGroup.put("/:id", (req, res) =>
-        patientController.updatePatient(req, res)
-    );
+  patientGroup.put("/:id", (req, res) =>
+    patientController.updatePatient(req, res)
+  );
 
-    patientGroup.delete("/:id", (req, res) =>
-        patientController.deletePatient(req, res)
-    );
+  patientGroup.delete("/:id", (req, res) =>
+    patientController.deletePatient(req, res)
+  );
 
-    router.use("/patients", patientGroup);
+  router.use("/patients", patientGroup);
 }
